994ad6d44d4c5319daaf6ec93a24ce348be30b71
marquirj <udt.popes@gmail.com>
2021-08-03 19:18:40 +0200

    editar

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -1,14 +1,14 @@
 import React, {useState} from 'react'
 import Proptypes from 'prop-types';
 export const AddCategory = ({setCategories}) => {
-    const [inputValue, setInputValue] = useState('Hola Mundo');
+    const [inputValue, setInputValue] = useState('');
     const handleInputChange = (e) => {
         setInputValue(e.target.value);
     }
-    const handleSubmit= (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
         if(inputValue.trim().length > 2){
-            setCategories(cats=>[inputValue,...cats]);
+            setCategories(cats => [inputValue,...cats, ]);
             setInputValue('');
         }
         
